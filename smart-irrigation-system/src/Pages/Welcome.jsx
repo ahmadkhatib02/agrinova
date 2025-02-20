8c112d79bc08a9cb86c6ebc4f0f0a7a8aed8fdf1
ahmadkhatib02 <ahmad.alkhatib02@lau.edu>
2025-02-20 12:58:26 +0200

    added comments for more simplicity approach

--- smart-irrigation-system/src/Pages/Welcome.jsx
+++ smart-irrigation-system/src/Pages/Welcome.jsx
@@ -1,3 +1,8 @@
+// CSS stylesheet
+import "../styles/WelcomePage.css"
+
+//Import your components here (make sure that they are saved in components folder)
+
 export default function Welcome () {
     return (
         <>
