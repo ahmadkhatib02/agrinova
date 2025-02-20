75e2e3282cc16aaa91bdecc42d14467941bcc29e
ahmadkhatib02 <ahmad.alkhatib02@lau.edu>
2025-02-20 12:57:47 +0200

    added the front page to be edited

--- smart-irrigation-system/src/App.jsx
+++ smart-irrigation-system/src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import HomePage from "./Pages/HomePage"
+import Welcome from "./Pages/Welcome"
 
 function App() {
   
   return (
     <Router>
     <Routes>
-      <Route path="/" element={<HomePage />} />
+      <Route path="/homePage" element={<HomePage />} />
+      <Route path="/" element ={<Welcome />}/>
     </Routes>
   </Router>
   )
