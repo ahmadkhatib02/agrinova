3d54283512395ba876c2c065d1e3a36fb51ce6c2
ahmadkhatib02 <ahmad.alkhatib02@lau.edu>
2025-04-02 21:32:36 +0300

    fixed the Footer Nav bar

---
+++ smart-irrigation-system/src/Pages/Account.jsx
@@ -0,0 +1,12 @@
+import Footer from "../components/Footer";
+
+
+export default function Account () {
+
+    return (
+        <>
+            <h1>Account Page</h1>
+            <Footer page ="account"/>
+        </>
+    )
+}
